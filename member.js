4c56ec717477fa9f98e6b34fb2f20b8c7bea3e79
cristiansoto-f <56857490+cristiansoto-f@users.noreply.github.com>
2024-01-02 03:50:43 +0000

    Copilot new commit

--- member.js
+++ member.js
@@ -1,3 +1,39 @@
 function skillsMember() {
-    console.log('skillsMember');
+    var name = "Sia";
+    var age = 20;
+    var skills = ["Javascript", "HTML", "CSS"];
+    var address = "Bogor";
+    var is_married = false;
+    var list_school = [
+        {
+            name: "SMAN 1 Bogor",
+            year_in: 2015,
+            year_out: 2018,
+            major: "IPA"
+        },
+        {
+            name: "SMPN 1 Bogor",
+            year_in: 2012,
+            year_out: 2015,
+            major: null
+        },
+        {
+            name: "SDN 1 Bogor",
+            year_in: 2006,
+            year_out: 2012,
+            major: null
+        }
+    ];
+    var interest_in_coding = true;
+
+    var json = {
+        name,
+        age,
+        skills,
+        address,
+        is_married,
+        list_school,
+        interest_in_coding
+    };
+    return JSON.stringify(json);
 }
